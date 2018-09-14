b9c97e8703dcd6c287bcd316a5f5fb95053154de
Andy Le <leal1@uci.edu>
2018-09-14 11:38:08 -0700

    Fixed buttons, styled new quote, added footer

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-$(document).ready(function() {
 	function addQuote(){
 	    $.ajax( {
 	      
@@ -11,7 +10,15 @@ $(document).ready(function() {
 
 	      success:function(json){
 	      	document.getElementById("text").innerHTML = json.quoteText;
-	      	document.getElementById("author").innerHTML = json.quoteAuthor;
+	      	console.log('ya');
+	      	if(json.quoteAuthor==""){
+	      		document.getElementById("author").innerHTML = "Unknown";
+	      	}
+	      	else{
+	      		document.getElementById("author").innerHTML = json.quoteAuthor;
+	      	}
+
+	      	
 	      },
 	      error:function()
 	      {
@@ -20,11 +27,25 @@ $(document).ready(function() {
 	      }    
 	    })
 	}
+$(document).ready(function() {
+
 	addQuote();
 	$('#new-quote').click(function(e) {
 	    e.preventDefault();
 	    addQuote();
-	  })
+	  });
+	$('#share_tweet').click(function(e){
+		e.preventDefault();
+		window.open('https://twitter.com/intent/tweet?text='+ '"' + 
+			$('#text').text() + '" -' + $('#author').text());
 
+
+	});
+	$('#login_tumblr').click(function(e){
+		e.preventDefault();
+		window.open('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' 
+			+ $('#author').text() + '&content=' + $('#text').text() 
+			+ '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button')
+	});
 
 });
